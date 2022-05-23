8bad92e291d1986f1d0c8d34c20463ded2c8d17c
Thies Nieborg <thies.nieborg@student.uva.nl>
2022-05-23 13:13:27 +0200

    Added a flake8 test

---
+++ test_flake8.py
@@ -0,0 +1,10 @@
+import subprocess
+
+
+def test_flake8():
+    res = subprocess.Popen(
+        ["flake8", "--max-line-length=120", "--ignore=E203,W503", "--exclude=tests"],
+        stdout=subprocess.PIPE,
+    ).stdout.read()
+    print(res)
+    assert len(res) == 0 and "Flake8 outputted something"

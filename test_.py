ee4ca46f7ef5e44089e900749d87691d3b20407c
Luuk Jonker <luuk.jonker@student.uva.nl>
2022-05-23 21:36:02 +0200

    First append tests

--- test_.py
+++ test_.py
@@ -3,8 +3,10 @@ import subprocess
 import os
 import pytest
 
-program_path = os.path.abspath(".") + "/mfstool.py"
+cwd = os.path.abspath(".")
+program_path = cwd + "/mfstool.py"
 test_folder = "minix_tests"
+test_path = cwd + '/' + test_folder + '/'
 
 ls_working = False
 
@@ -35,15 +37,36 @@ class TestCat():
         assert cat(test_folder + "/multiple_blocks_cat.img", 'bee_movie.txt') == results.bee
         assert cat(test_folder + "/multiple_blocks_cat.img", 'diri/shrek.txt') == results.shrek
 
+    def test_cat_removed(self):
+        assert cat(test_folder + "/cat_removed.img", 'removed.txt') == results.removed_result
+
+    def test_cat_empty(self):
+        assert cat(test_folder + "/cat_empty.img", "empty.txt") == b''
+
 class TestTouch():
     def test_touch(self):
         if not ls_working:
-            exit("Sorry, but we can't test the rest without a aworking ls")
+            exit("Sorry, but we can't test the rest without a working ls")
         assert touch() == results.touch_result
 
 class TestMkdir():
     def test_mkdir(self):
         assert mkdir() == results.mkdir_result
+
+class TestAppend():
+    def test_append_single(self):
+        assert append('append_small.img', results.append_in) == results.append_out
+
+    def test_append_multiple(self):
+        assert append('append_small.img', read_file('shrek.txt')) == read_file('shrek.txt')
+
+    def test_append_big(self):
+        assert append('append_bigboy.img', read_file('shrek.txt')) == read_file('shrek.txt')
+
+def read_file(file):
+    with open(test_path + file, 'rb') as f:
+        return f.read()
+
 
 def ls(file):
     proc = subprocess.Popen(['python3', program_path, file, 'ls'], stdout=subprocess.PIPE)
@@ -70,3 +93,17 @@ def mkdir():
     subprocess.run(['python3', program_path, '/tmp/mkdir.img', 'mkdir', "diri"])
 
     return ls('/tmp/mkdir.img')
+
+def append(file, content):
+    subprocess.run(['cp', test_path + file, '/tmp/' + file])
+
+    subprocess.run(['python3', program_path, '/tmp/' + file, 'append', "file.txt", content])
+
+    subprocess.run(['sudo', 'mount', '-o', 'loop', '/tmp/' + file, '/mnt'])
+
+    proc = subprocess.Popen(['sudo', 'cat', '/mnt/file.txt'], stdout=subprocess.PIPE)
+    out = proc.stdout.read()
+
+    subprocess.run(['sudo', 'umount', '/mnt'])
+
+    return out
